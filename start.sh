0d16d637db1def79627bbc75e6488e09e1f8ab51
yangsoosong <song.yang@husky.neu.edu>
2019-01-25 01:23:43 -0500

    Edit start.sh before deployment

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
+#if [[ "x$PROD" == "x" ]]; then 
+#	echo "This script is for starting in production."
+#	echo "Use"
+#	echo "   mix phx.server"
+#	exit
+#fi
 
 # TODO: Enable this script by removing the above.
 
@@ -18,7 +18,7 @@ _build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice foreground
+_build/prod/rel/practice/bin/practice start 
 
 # TODO: Change "foreground" to "Start"
 
